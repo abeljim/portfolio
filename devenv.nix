68e5b6ebb80a4f8649e6a811fe833383b528437c
Abel Jimenez <abelj1@uci.edu>
2024-05-19 17:46:54 -0700

    added prettier to devenv

--- devenv.nix
+++ devenv.nix
@@ -2,13 +2,14 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  # env.GREET = "devenv";
+
 
   # https://devenv.sh/packages/
   # packages = [ pkgs.git ];
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+  scripts.hello.exec = "echo Entering Portfolio Enviroment";
 
   enterShell = ''
     hello
@@ -35,6 +36,7 @@
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks.prettier.enable = true;
 
   # https://devenv.sh/processes/
   # processes.ping.exec = "ping example.com";
